390b6deabd27043d83179004a67a7f81f91b234d
github username <github email address>
2018-09-27 09:28:09 -0400

    Done.

--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,11 @@
 # Implement your procedural solution here!
+
+def sum_square_difference(num)
+  sum = 0
+  squares = 0
+  (1..num).to_a.each do |n|
+    sum += n
+    squares += n**2
+  end
+  return (sum**2 - squares)
+end
